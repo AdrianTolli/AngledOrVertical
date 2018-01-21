c2edd8ec14c804d696a465d71d1d36cffa8c62cd
AdrianTolli <adrian.tollisen@hotmail.com>
2018-01-21 22:02:07 +0100

    Cpoied files from herokubranch

--- main.js
+++ main.js
@@ -10,32 +10,45 @@ $(document).ready(function(){
         $('.highlight').removeClass('highlight');
         $('.rmb').addClass('highlight');
         aimMode = "rmb";
+
+        $('.info').text("You've chosen Right Mouse Button, now click on desired gun");
     });
 
     $('#aimSelector').on('click', '.ads', function(){
         $('.highlight').removeClass('highlight');
         $('.ads').addClass('highlight');
         aimMode="ads";
+        $('.info').text("You've chosen Aim Down Sight, now click on desired gun");
     });
 
     $('#gunText').on('click', '.m4Text', function() {
-        $('.angledImg').attr('src', './images/m4_angled_'+aimMode+'.jpg');
-        $('.verticalImg').attr('src', './images/m4_vertical_'+aimMode+'.jpg');
+        $('.angledImg').attr('src', './images/m4_angled_'+aimMode+'.png');
+        $('.verticalImg').attr('src', './images/m4_vertical_'+aimMode+'.png');
+        $('.info').text("You've chosen M416! No additional information about this weapon. Other attachments will be an option soon!");
     });
 
     $('#gunText').on('click', '.scarText', function() {
-        $('.angledImg').attr('src', './images/scar_angled_'+aimMode+'.jpg');
-        $('.verticalImg').attr('src', './images/scar_vertical_'+aimMode+'.jpg');
+        $('.angledImg').attr('src', './images/scar_angled_'+aimMode+'.png');
+        $('.verticalImg').attr('src', './images/scar_vertical_'+aimMode+'.png');
+        $('.info').text("You've chosen Scar! No additional information about this weapon. Other attachments will be an option soon");
     });
 
     $('#gunText').on('click', '.sksText', function() {
-        $('.angledImg').attr('src', './images/m4_angled_'+aimMode+'.jpg');
-        $('.verticalImg').attr('src', './images/m4_vertical_'+aimMode+'.jpg');
+        $('.angledImg').attr('src', './images/sks_angled_'+aimMode+'.png');
+        $('.verticalImg').attr('src', './images/sks_vertical_'+aimMode+'.png');
+        $('.info').text("You've chosen SKS! Since this is not an autofire weapon, this is just spamclick. Aim Down Sight pictures will be added shortly!");
     });
 
     $('#gunText').on('click', '.umpText', function() {
-        $('.angledImg').attr('src', './images/ump_angled_'+aimMode+'.jpg');
-        $('.verticalImg').attr('src', './images/ump_vertical_'+aimMode+'.jpg');
+        $('.angledImg').attr('src', './images/ump_angled_'+aimMode+'.png');
+        $('.verticalImg').attr('src', './images/ump_vertical_'+aimMode+'.png');
+        $('.info').text("You've chosen UMP! No additional information about this weapon. Other attachments will be an option soon");
+    });
+
+    $('#gunText').on('click', '.vectorText', function() {
+        $('.angledImg').attr('src', './images/vector_angled_'+aimMode+'.png');
+        $('.verticalImg').attr('src', './images/vector_vertical_'+aimMode+'.png');
+        $('.info').text("You've chosen Vector! This weapon cannot equip angled grip. Other attachments will be an option soon");
     });
 
 })
